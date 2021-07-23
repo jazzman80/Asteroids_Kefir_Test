eae8996105ae81c1ab7557c1e73289e08b7f79bd
jazzman80 <77989227+jazzman80@users.noreply.github.com>
2021-07-24 00:10:47 +0300

    UFO art and move

---
+++ Assets/Scripts/UFO.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UFO : MonoBehaviour
+{
+    [SerializeField] private Rigidbody body;
+    [SerializeField] private float movingSpeed;
+    [SerializeField] private float rotationSpeed;
+    [SerializeField] Transform target;
+
+    private void FixedUpdate()
+    {
+        float targetDirection = Vector3.SignedAngle(transform.forward, target.position - transform.position, transform.up);
+
+        body.AddTorque(transform.up * rotationSpeed * Time.fixedDeltaTime * Mathf.Sign(targetDirection));
+        
+        body.AddForce(transform.forward * movingSpeed * Time.fixedDeltaTime);
+    }
+
+}
