2820cbe80721e8bb5532ab0bf7a6776b5b67dbdf
jazzman80 <77989227+jazzman80@users.noreply.github.com>
2021-07-29 23:52:20 +0300

    New laser gun logic

--- Assets/Scripts/LaserGun.cs
+++ Assets/Scripts/LaserGun.cs
@@ -4,24 +4,36 @@ using UnityEngine;
 public class LaserGun : Gun
 {
     [SerializeField] float chargeTime;
+    [SerializeField] int maximumCharge;
+    int charge;
     bool isCharged = true;
+
+    private void Start()
+    {
+        charge = maximumCharge;
+    }
 
     public override bool CheckFire()
     {
-        if (Input.GetButtonDown(fireButton) && isCharged) return true;
+        if (Input.GetButtonDown(fireButton) && charge > 0) return true;
         else return false;
     }
 
     public override void Fire(Ammo ammo)
     {
         ammo.Fire(transform, fireForce);
-        isCharged = false;
+        //isCharged = false;
+        if(charge > 0) charge--;
+
+        StopAllCoroutines();
         StartCoroutine(Charge());
     }
 
     IEnumerator Charge()
     {
         yield return new WaitForSeconds(chargeTime);
-        isCharged = true;
+        //isCharged = true;
+        charge++;
+        if (charge < maximumCharge) StartCoroutine(Charge());
     }
 }
