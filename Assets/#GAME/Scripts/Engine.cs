c53ef4450e092bc44908f119bcdbfcdaab4a6df0
jazzman80 <77989227+jazzman80@users.noreply.github.com>
2021-08-02 23:52:53 +0300

    3D models and particle fire

--- Assets/#GAME/Scripts/Engine.cs
+++ Assets/#GAME/Scripts/Engine.cs
@@ -3,10 +3,19 @@ using UnityEngine;
 public class Engine : MonoBehaviour
 {
     [SerializeField] GameObject fire;
+    [SerializeField] ParticleSystem particleFire;
 
     private void Update()
     {
-        if (!fire.activeSelf && Input.GetAxis("Vertical") > 0) fire.SetActive(true);
-        if (fire.activeSelf && Input.GetAxis("Vertical") == 0) fire.SetActive(false);
+        if (!fire.activeSelf && Input.GetAxis("Vertical") > 0)
+        {
+            fire.SetActive(true);
+            particleFire.Play();
+        }
+        if (fire.activeSelf && Input.GetAxis("Vertical") == 0)
+        {
+            fire.SetActive(false);
+            particleFire.Stop();
+        }
     }
 }
